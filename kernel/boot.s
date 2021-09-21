c4572bd04d27a10ca62e5c83eb007819439dd919
Piotro <piotro888@wp.pl>
2021-09-21 20:48:58 +0200

    Start of piOS system

---
+++ kernel/boot.s
@@ -0,0 +1,22 @@
+.romd
+
+.rod magicnumber, 0xCAFE
+
+; begin of piOS ^^
+
+_startpoint:
+	; set up initial stack
+	ldi r7, 0xFFFF
+	ldi r5, 0xFFFF
+	
+	; mark r0
+	ldi r0, 0x314
+	
+	; call _start in kstart.c
+	jal r6, _kstart	
+
+	; kernel should not return
+	; error code
+	ldi r0, 0xAF
+	ASMerr_loop:
+		jmp ASMerr_loop
---
+++ kernel/kstart.c
@@ -0,0 +1,9 @@
+/* 
+ * piOS kernel entry point
+ * (C) 2021 by Piotr Wegrzyn 
+*/
+
+/* C entry point for kernel */
+void _kstart(){
+
+}
