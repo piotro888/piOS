ce2ef32d2717148fd3cd639f09842c699dd31358
Piotro <piotro888@wp.pl>
2021-10-30 11:38:07 +0200

    basic os test

--- kernel/irq/interrupt.c
+++ kernel/irq/interrupt.c
@@ -2,14 +2,15 @@
 
 /* Interrupt handler called from irq.s */
 void interrupt() {
-    kprintf("irq\n");
+    int scancode = *((int*)3);
+    kprintf("irq scan:%d\n", scancode);
 }
 
 /* Enable interrupts in CPU */
 void int_enable() {
     asm volatile (
         "srl r0, 1\n"
-        "ani r0, r0, 0x4\n"
+        "ori r0, r0, 0x4\n"
         "srs r0, 1\n"
         ::: "r0"
     );
--- kernel/kstart.c
+++ kernel/kstart.c
@@ -4,16 +4,22 @@
 */
 
 #include <video/tty.h>
+#include <libk/kprintf.h>
+#include <irq/interrupt.h>
 
 /* C entry point for kernel */
-void _kstart(){
-   
+void _kstart() {
     init_tty();
-    while(1) {
-        tty_putc('a');
-        tty_putc('b');
-        tty_putc('c');
-        tty_putc('d');
-        tty_putc('e');
-    }
+    tty_set_color((char)0x02 | 0x08);
+    tty_puts("pios");
+    tty_set_color((char)0x0F);
+    tty_puts(" kernel booting\n");
+    tty_putc('\n');
+    tty_putc('a');
+    tty_putc('b');
+    tty_putc('c');
+    kprintf("cab");
+    kprintf("int%db%db%db%db%db%dc", 1287, 33, 888, 3, 5, 9);
+    kprintf("okaaaaaaaaaaaaaaaaaaa");
+    int_enable();
 }
