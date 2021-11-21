e153915816c668d20dbc2ae854927479c8c1a403
Piotro <piotro888@wp.pl>
2021-11-21 23:37:33 +0100

    Kstart: print status logs during boot

--- kernel/kstart.c
+++ kernel/kstart.c
@@ -4,9 +4,9 @@
 */
 
 #include <driver/tty.h>
-#include <libk/kprintf.h>
 #include <irq/interrupt.h>
 #include <libk/kmalloc.h>
+#include <libk/kprintf.h>
 
 /* C entry point for kernel */
 void _kstart() {
@@ -16,20 +16,10 @@ void _kstart() {
     tty_set_color((char)0x0F);
     tty_puts(" kernel booting\n");
     tty_putc('\n');
-    tty_putc('a');
-    tty_putc('b');
-    tty_putc('c');
-    kprintf("cab");
-    kprintf("int%db%db%db%db%db%dc", 1287, 33, 888, 3, 5, 9);
-    kprintf("okaaaaaaaaaaaaaaaaaaa");
-    int_enable();
+    kprintf("initializing kernel heap\n");
     init_malloc();
-    void* ptr1, *ptr2, *ptr3, *ptr4, *ptr5;
-    ptr1 = kmalloc(100);
-    ptr2 = kmalloc(2);
-    kfree(ptr1);
-    ptr3 = kmalloc(140);
-    ptr4 = kmalloc(20);
-    ptr5 = kmalloc(64);
-    kprintf("ptr  %x %x %x %x %x", ptr1, ptr2, ptr3, ptr4, ptr5);
+    kprintf("enabling interrupts\n");
+    int_enable();
+    kprintf("init done.\n");
+    search_file();
 }
