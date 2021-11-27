76ffdea14871a939db72cdc194a913c8a4252182
Piotro <piotro888@wp.pl>
2021-11-27 09:58:19 +0100

    Libk: Fix string compare

--- kernel/libk/string.c
+++ kernel/libk/string.c
@@ -1,8 +1,8 @@
 #include "string.h"
 
 size_t strcmp(char* str1, char* str2) {
-    while(*str1++ == *str2++) {
-        if(*str1 == '\0' && *str2 == '\0')
+    while(*str1 == *str2) {
+        if(*str1++ == 0 && *str2++ == 0)
             return 0;
     }
     return *str1-*str2;
@@ -12,11 +12,12 @@ size_t strncmp(char* str1, char* str2, size_t n) {
     if(!n)
         return 0;
 
-    int pos = 1;
-    while(*str1++ == *str2++) {
-        if(*str1 == '\0' && *str2 == '\0')
+    size_t pos = 0;
+    while(*str1 == *str2) {
+        if(*str1++ == 0 && *str2++ == 0)
             return 0;
-        if(pos++ == n)
+
+        if(++pos >= n)
             return 0;
     }
     return *str1-*str2;
