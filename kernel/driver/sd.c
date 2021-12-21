d20a433b2124e487cdaf54ebaff1e1a5f6cf4613
Piotro <piotro888@wp.pl>
2021-12-21 21:36:06 +0100

    SD: Refactor SD commands and finish correct initalization

--- kernel/driver/sd.c
+++ kernel/driver/sd.c
@@ -1,7 +1,6 @@
 #include "sd.h"
 
 #include <driver/spi.h>
-#include <libk/types.h>
 #include <libk/kprintf.h>
 #include <panic.h>
 
@@ -10,31 +9,25 @@
 #define CMD0 0
 #define CMD0_CRC 0x4A
 #define CMD8 8
+#define CMD8_PARAM 0x1AA
 #define CMD8_CRC 0x43
+#define ACMD_PRE_CMD55 55
+#define ACMD41 41
+#define ACMD41_ARG_HC 0x4000
+#define CMD58 58
 
+#define STATUS_NULL 0x0
 #define STATUS_IDLE 0x1
-#define STATUS_ILL 0x3
+#define STATUS_ILLEGAL_CMD 0x5
 
-// ak <3
+#define SD_RESP_TIMEOUT 8
+#define SD_INIT_TIMEOUT 16
 
-/* 
-    TODO:
-    assert responses
-    timeouts
-    separate functions for r1, r3/7 (without waiting)
-*/
+#define OCR_1_3V3 0x10
+#define OCR_0_HC 0x80
+#define CMD8_3_VOLATAGE 0x1
 
-void sd_get_response(uint8_t* buff, size_t len) { 
-    while(len--) {
-        int response = 0xFF;
-        while(response == 0xFF)
-            response = spi_receive();
-        *buff = response;
-        buff++;   
-    }
-}
-
-uint8_t sd_command(uint8_t index, uint16_t arg_upper, uint16_t arg_lower, uint8_t crc) {
+void sd_command(uint8_t index, uint16_t arg_upper, uint16_t arg_lower, uint8_t crc, uint8_t* resp_buff, uint8_t resp_size) {
     spi_transmit(0x40 | index);
     spi_transmit(arg_upper >> 8);
     spi_transmit(arg_upper & 0xFF);
@@ -42,44 +35,52 @@ uint8_t sd_command(uint8_t index, uint16_t arg_upper, uint16_t arg_lower, uint8_
     spi_transmit(arg_lower & 0xFF);
     spi_transmit((crc << 1) | 1);
 
-    uint8_t response;
-    sd_get_response(&response, 1);
+    // wait for first byte
+    uint8_t response = 0xFF, timeout = SD_RESP_TIMEOUT; 
+    while(response == 0xFF) {
+        response = spi_receive();
+        if(--timeout == 0)
+            panic("driver/sd: command timeout");
+    }
 
-    return response;
-}
+    *resp_buff = response;
+    // R3/R7 responses
+    for(int i=0; i<resp_size-1; i++) {
+        *(++resp_buff) = spi_receive();
+    }
 
-void sd_end_command() {
-    spi_transmit(0xFF); // some cards need additional 0xFF after command 
+    spi_transmit(0xFF); // some cards need additional byte after command
 }
 
 void sd_hc_init() {
     uint8_t r;
-    kprintf("SD HC CARD DETECTED\n");
-    
-    for(int i=0; i<4; i++) {
-        kprintf("%x ", spi_receive());
-    }
-    sd_end_command();
+    uint8_t rb[5];
 
     // ACMD41 sequence with HCSD arg
-    while (1) {
-        r = sd_command(55, 0, 0, 0);
-        sd_end_command();
-        kprintf("cmd55 response : 0x%x\n", r);
-        r = sd_command(41, 0x4000, 0, 0);
-        sd_end_command();
-        kprintf("cmd41 response : 0x%x\n", r);
-        if(r == 0)
+    uint8_t timeout = SD_RESP_TIMEOUT;
+    while (timeout--) {
+        sd_command(ACMD_PRE_CMD55, 0, 0, 0, &r, 1);
+        if(r != STATUS_IDLE)
+            panic("driver/sd: illegal response to cmd55 (acmd41)");
+        
+        sd_command(ACMD41, ACMD41_ARG_HC, 0, 0, &r, 1);
+        if(r == STATUS_NULL) // card initialized (exited idle state)
             break;
+        else if(r != STATUS_IDLE)
+            panic("driver/sd: illegal response to acmd41");
     }
 
-    r = sd_command(58, 0, 0, 0);
-    sd_end_command();
-    kprintf("response : 0x%x\n", r);
+    sd_command(CMD58, 0, 0, 0, rb, 5);
+
+    if(!(rb[1] | OCR_1_3V3))
+        panic("driver/sd: volatage range not supported");
+    if(!(rb[0] | OCR_0_HC))
+        panic("driver/sd: not hc card");
 }
 
 void sd_init() {
     uint8_t r;
+    uint8_t rb[5];
     
     spi_cs_set(1, SD_DEVICE_NO); // disable chip select
     for(int i=0; i<74; i++) {
@@ -88,23 +89,24 @@ void sd_init() {
     spi_cs_set(0, SD_DEVICE_NO);
     // spi mode enabled
 
-    r = sd_command(CMD0, 0, 0, CMD0_CRC);
-    sd_end_command();
-    if(r != 0x1) {
+    sd_command(CMD0, 0, 0, CMD0_CRC, &r, 1);
+    if(r != STATUS_IDLE) {
         kprintf("driver/sd: invalid response to CMD0: %d", r);
         panic("driver/sd: sd card failed to initialize");
     }
-    kprintf("sd intit stage1 ok\n");
 
-    r = sd_command(CMD8, 0, 0x1AA, CMD8_CRC);
-    if(r == 0x1) {
+    sd_command(CMD8, 0, CMD8_PARAM, CMD8_CRC, rb, 5);
+    if(rb[0] == STATUS_IDLE) {
+        if(rb[3] != CMD8_3_VOLATAGE || rb[4] != (CMD8_PARAM&0xff)) // voltage range and check pattern
+            panic("driver/sd: invalid response to cmd8");
+
         sd_hc_init();
-    } else if (r == 0x5) {
-        //sd_lc_init();
+    } else if (rb[0] == STATUS_ILLEGAL_CMD) {
+        panic("driver/sd: low capacity cards not implemented");
     } else {
-        kprintf("driver/sd: invalid response to CMD8: 0x%x\n", r);
+        kprintf("driver/sd: invalid response to CMD8: 0x%x\n", rb[0]);
         panic("driver/sd: sd card failed to initialize");
     }
     
-    kprintf("driver/sd: sd card initialized");
+    kprintf("driver/sd: sd card initialized\n");
 }
