3719be461d555d84301485745ac80fb760f0d6e0
Piotro <piotro888@wp.pl>
2022-01-28 15:39:49 +0100

    Kernel: Better panic message

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,6 +1,12 @@
+#include "panic.h"
 #include <libk/kprintf.h>
+#include <driver/tty.h>
 
 void panic(char* message) {
-    kprintf("kernel panic  %s", message);
+    tty_set_color(0x9F);
+    kprintf("\n[!] KERNEL PANIC\n");
+    tty_set_color(0xF);
+    kprintf("Caused by: %s\n", message);
+    kprintf("*kernel halted*");
     while(1);
 }
