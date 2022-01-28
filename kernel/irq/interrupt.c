eef2b6b659362eeb227ddebf7791d0101cdb0468
Piotro <piotro888@wp.pl>
2022-01-28 15:42:04 +0100

    IRQ: Support interrupts from userspace
    Assembly script uses saved kernel interrupt stack.
    State of process is passed to c function and saved to proc structure.
    Interrupts are mainly exited by switch_to_userspace function now, which
    supports virtual memory

--- kernel/irq/interrupt.c
+++ kernel/irq/interrupt.c
@@ -1,11 +1,45 @@
 #include <libk/kprintf.h>
 #include <driver/keyboard.h>
+#include <libk/assert.h>
+#include <proc/sched.h>
+#include <proc/proc.h>
+#include <proc/virtual.h>
 
 /* Interrupt handler called from irq.s */
-void interrupt() {
+__attribute__((used))
+void interrupt(char* state) {
+    kprintf("[interrupted] ptr=%x pc=%x r0=%d r1=%d r2=%d pc+1=%d pc-1=%d  ", (int)state+16, *(int*)((int)state+16-11), *(int*)(state+16-3), *(int*)(state+16-4), *(int*)(state+16-5), *(int*)(state+16-10), *(int*)(state+16-12));
+
+    // save thread state
+    current_proc.regs[0] = *(int*)(state+16-3);
+    current_proc.regs[1] = *(int*)(state+16-4);
+    current_proc.regs[2] = *(int*)(state+16-5);
+    current_proc.regs[3] = *(int*)(state+16-6);
+    current_proc.regs[4] = *(int*)(state+16-7);
+    current_proc.regs[5] = *(int*)(state+16-8);
+    current_proc.regs[6] = *(int*)(state+16-9);
+    current_proc.regs[7] = *(int*)(state+16-10);
+    current_proc.pc =      *(int*)(state+16-11);
+    current_proc.arith_flags = *(int*)(state+16-12);
+
+    kprintf("\nState saved regs = %d %d %d %d pc = %d\n", current_proc.regs[0], current_proc.regs[1], current_proc.regs[2], current_proc.regs[3], current_proc.pc);
+
     uint8_t scancode = *((int*)3);
     //kprintf("irq scan:%d\n", scancode);
     print_scancode(scancode);
+
+    if(scheduling_enabled) {
+        // resume execution from current thread
+        kprintf("[ie to thread %d]", current_proc.pid);
+        ASSERT(current_proc.state != PROC_STATE_UNLOADED);
+        switch_to_userspace(&current_proc);
+    }
+
+    kprintf("[ie]  ");
+    /*  allow only returning here with virtual memory disabled
+        i.e. before scheduling (when kernel is booting before threads and initializing)
+    */ 
+    ASSERT(!scheduling_enabled);
 }
 
 /* Enable interrupts in CPU */
--- kernel/irq/irq.s
+++ kernel/irq/irq.s
@@ -2,10 +2,14 @@
 
 ; interrupt vector (0x01 @ boot.s) jumps here
 interrupt_handler:
-    ; user space not yet supported - interrupts from kernel space use r7 as kernel sp
+    ; interrutps from userspace are supported; kernel and userspace interrupts use single kernel stack loaded from memory
     ; start from -3 to not break prologue/epilogue
 
     ; save registers
+    std r7, r7_temp_location ; store r7 to temporary location
+    ; load trusted kernel stack pointer (but because of that no nested irqs allowed)
+    ldd r7, interrupt_sp
+    ; save registers to stack
     sto r0, r7, -3
     sto r1, r7, -4
     sto r2, r7, -5
@@ -13,25 +17,41 @@ interrupt_handler:
     sto r4, r7, -7
     sto r5, r7, -8
     sto r6, r7, -9
+    ; save r7
+    ldd r0, r7_temp_location
+    sto r0, r7, -10
 
     ; save interrupted program counter
     srl r0, 3
-    sto r0, r7, -10
+    sto r0, r7, -11
     ; save arithmetic flags (before any arith operation!)
     srl r0, 4
-    sto r0, r7, -11
-    ; adjust sp
-    adi r7, r7, -12
+    sto r0, r7, -12
+    ; load special flags usefull for kernel
+    srl r0, 5
+    sto r0, r7 -13
+    ; adjust sp (+ space for passing argument)
+    adi r7, r7, -16
     ; state saved
 
+    ; pass pointer to state as argument
+    mov r0, r7
+
     jal r6, interrupt ; call kernel interrupt handler
+    
+    ; NEVER REACHED. Interrupts should be disabled before sheduler
+    ; and we always return to some other thread but not to irq thread (no virtual) when enabled
 
     ; return from interrupt
-    
-    adi r7, r7, 12
-    ldo r0, r7, -11
+    ; but only to kernel (and only main thread, kernel threads also need virutal memory)
+    ; returns to userspace via proc/virtual/switch_to_userspace
+    ; so this path will be only used in interrupts before scheduler
+    ; in that case we could just resume from data stored on stack
+
+    adi r7, r7, 16
+    ldo r0, r7, -12
     srs r0, 4 ; arithmetic flags
-    ldo r0, r7, -10
+    ldo r0, r7, -11
     srs r0, 3 ; return program counter
     ; restore registers
     ldo r0, r7, -3
@@ -41,5 +61,11 @@ interrupt_handler:
     ldo r4, r7, -7
     ldo r5, r7, -8
     ldo r6, r7, -9
+    ldo r7, r7, -10 ; we leave `IRQ stack` now
     ; return from interrupt
     irt
+
+.ramd
+; stack pointer value loaded after interrupt in kernel
+.init interrupt_sp, 0xEFF0
+.global r7_temp_location, 1
