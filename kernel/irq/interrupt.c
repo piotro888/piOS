bd7a79a0193463c13c0a2bddad3c3e8f63b68c4c
Piotro <piotro888@wp.pl>
2022-02-17 19:12:27 +0100

    IRQ: Interrupt controller support
    Use interrupt controller to handle multiple interrutps and system calls

--- kernel/irq/interrupt.c
+++ kernel/irq/interrupt.c
@@ -6,34 +6,40 @@
 #include <proc/proc.h>
 #include <proc/virtual.h>
 
+#define IRQ_CLEAR(x)   (*(volatile u16*) 0x12) = (1<<(x))
+#define IRQ_PENDING(x) (*(volatile u16*) 0x10) & (1<<(x)) 
+
 /* Interrupt handler called from irq.s */
 __attribute__((used))
-void interrupt(char* state) {
-    kprintf("[interrupted] ptr=%x pc=%x r0=%d r1=%d r2=%d pc+1=%d pc-1=%d  ", (int)state+16, *(int*)((int)state+16-11), *(int*)(state+16-3), *(int*)(state+16-4), *(int*)(state+16-5), *(int*)(state+16-10), *(int*)(state+16-12));
+void interrupt(const char* state) {
+    kprintf("[interrupted]\n");
 
     // save thread state
-    current_proc.regs[0] = *(int*)(state+16-3);
-    current_proc.regs[1] = *(int*)(state+16-4);
-    current_proc.regs[2] = *(int*)(state+16-5);
-    current_proc.regs[3] = *(int*)(state+16-6);
-    current_proc.regs[4] = *(int*)(state+16-7);
-    current_proc.regs[5] = *(int*)(state+16-8);
-    current_proc.regs[6] = *(int*)(state+16-9);
-    current_proc.regs[7] = *(int*)(state+16-10);
-    current_proc.pc =      *(int*)(state+16-11);
-    current_proc.arith_flags = *(int*)(state+16-12);
+    for(int i=0; i<8; i++)
+        current_proc->regs[i] = *(int*)(state+16-(3+i));
+    current_proc->pc =          *(int*)(state+16-11);
+    current_proc->arith_flags = *(int*)(state+16-12);
 
-    kprintf("\nState saved regs = %d %d %d %d pc = %d\n", current_proc.regs[0], current_proc.regs[1], current_proc.regs[2], current_proc.regs[3], current_proc.pc);
 
-    uint8_t scancode = *((int*)3);
-    //kprintf("irq scan:%d\n", scancode);
-    print_scancode(scancode);
+    // check for syscall flag in sr
+    if((*(int*)(state+16-13)) & 0x8) {
+        kprintf("SYSCALL ");
+    }
+
+    // interrupt request and syscall could happen at same time
+    // clear interrupt pending from controller and process interrupts
+    if(IRQ_PENDING(0)) {
+        IRQ_CLEAR(0);
+        uint8_t scancode = *((int*)3);
+        print_scancode(scancode);
+    }
 
     if(scheduling_enabled) {
         // resume execution from current thread
-        kprintf("[ie to thread %d]", current_proc.pid);
-        ASSERT(current_proc.state != PROC_STATE_UNLOADED);
-        switch_to_userspace(&current_proc);
+        kprintf("[ie to thread %d]", current_proc->pid);
+        sched_pick_next();
+        ASSERT(current_proc->state != PROC_STATE_UNLOADED);
+        switch_to_userspace(current_proc);
     }
 
     kprintf("[ie]  ");
