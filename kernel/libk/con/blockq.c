635a3dec4d727e3c162ad517c1ca643838145004
Piotro <piotro888@wp.pl>
2022-05-06 12:07:59 +0200

    Libk: Add blocking queue

---
+++ kernel/libk/con/blockq.c
@@ -0,0 +1,47 @@
+#include "blockq.h"
+
+#include <libk/log.h>
+#include <libk/assert.h>
+
+void blockq_init(struct blockq* s, size_t size, size_t base_size) {
+    s->max_size = size;
+    s->base_size = base_size;
+
+    ringbuff_init(&s->rbuff, s->max_size*s->base_size);
+
+    semaphore_init(&s->not_empty);
+    semaphore_init(&s->not_full);
+    s->not_full.count = s->max_size;
+
+    spinlock_init(&s->read_lock);
+    spinlock_init(&s->write_lock);
+
+    s->size = 0;
+}
+
+void blockq_pop(struct blockq* s, void* buff) {
+    semaphore_down(&s->not_empty);
+
+    spinlock_lock(&s->read_lock);
+    ASSERT(s->size > 0);
+    ringbuff_read(&s->rbuff, buff, s->base_size);
+    s->size--;
+    spinlock_unlock(&s->read_lock);
+
+    semaphore_up(&s->not_full);
+}
+
+void blockq_push(struct blockq* s, void* buff) {
+    if(s->size == s->max_size)
+        log("blocking queue is full");
+
+    semaphore_down(&s->not_full);
+
+    spinlock_lock(&s->write_lock);
+    ringbuff_write(&s->rbuff, buff, s->base_size);
+    s->size++;
+    ASSERT(s->size <= s->max_size);
+    spinlock_unlock(&s->write_lock);
+
+    semaphore_up(&s->not_empty);
+}
---
+++ kernel/libk/con/blockq.h
@@ -0,0 +1,20 @@
+#ifndef LIBK_BLOCKQ_H
+#define LIBK_BLOCKQ_H
+
+#include <libk/con/semaphore.h>
+#include <libk/con/spinlock.h>
+#include <libk/ringbuff.h>
+#include <libk/types.h>
+
+struct blockq {
+    struct ringbuff rbuff;
+    struct semaphore not_empty, not_full;
+    size_t size, max_size, base_size;
+    struct spinlock read_lock, write_lock;
+};
+
+void blockq_init(struct blockq* s, size_t size, size_t base_size);
+void blockq_push(struct blockq* s, void* buff);
+void blockq_pop(struct blockq* s, void* buff);
+
+#endif
