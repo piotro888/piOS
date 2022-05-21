8b5a94fd4153cc0ad3a36ec0033c566929f2a3b1
Piotro <piotro888@wp.pl>
2022-05-21 14:31:38 +0200

    Proc: Add copy to/from userspace functions
    Working across page boundaries.
    And save mapping in map_page_zero(), to be recovered after interrupt

--- kernel/proc/virtual.c
+++ kernel/proc/virtual.c
@@ -1,6 +1,37 @@
 #include "virtual.h"
 
 #include <libk/assert.h>
+#include <libk/string.h>
+#include <libk/math.h>
+#include <proc/sched.h>
+
+#define PAGE_PART(addr) ((addr)>>12)
+#define ADDR_PART(addr) ((addr)&0x0fff)
+#define PHYS_PAGE(addr, page_table) ((page_table)[PAGE_PART(addr)])
+
+void memcpy_from_userspace(void* kbuff, struct proc* proc, u16 ubuff, size_t size) {
+    while (size) {
+        map_page_zero(PHYS_PAGE(ubuff, proc->mem_pages));
+        size_t to_copy = MIN(size, PAGE_SIZE - ADDR_PART(ubuff));
+        memcpy_full(kbuff, (void*)ADDR_PART(ubuff), to_copy);
+        size -= to_copy;
+        ubuff += to_copy;
+        kbuff = (void*) ((u16)kbuff+to_copy);
+    }
+    map_page_zero(0);
+}
+
+void memcpy_to_userspace(struct proc* proc, u16 ubuff, void* kbuff, size_t size) {
+    while (size) {
+        map_page_zero(PHYS_PAGE(ubuff, proc->mem_pages));
+        size_t to_copy = MIN(size, PAGE_SIZE - ADDR_PART(ubuff));
+        memcpy_full((void*)ADDR_PART(ubuff), kbuff, to_copy);
+        size -= to_copy;
+        ubuff += to_copy;
+        kbuff = (void*) ((u16)kbuff+to_copy);
+    }
+    map_page_zero(0);
+}
 
 extern void set_ram_mem(u16* data, int page, size_t size, size_t offset);
 void load_into_userspace(int page, u16* data, size_t size, size_t offset) {
@@ -29,6 +60,8 @@ void switch_to_userspace(struct proc* p) {
 }
 
 inline void map_page_zero(int page) {
+    current_proc->mem_pages[0] = page;
+
     asm volatile (
         "srs %0, 0x10"
         :: "r" (page)
--- kernel/proc/virtual.h
+++ kernel/proc/virtual.h
@@ -5,6 +5,9 @@
 
 #include <proc/proc.h>
 
+void memcpy_from_userspace(void* kbuff, struct proc* proc, u16 ubuff, size_t size);
+void memcpy_to_userspace(struct proc* proc, u16 ubuff, void* kbuff, size_t size);
+
 void load_into_userspace_program(int page, u16* data, size_t size, size_t offset);
 void load_into_userspace(int page, u16* data, size_t size, size_t offset);
 void switch_to_userspace(struct proc* p);
