b037dc05ba1a0e03df99331bd495412772cbe61a
Piotro <piotro888@wp.pl>
2022-05-21 14:30:09 +0200

    Libk: Add various memcpy functions

--- kernel/libk/string.h
+++ kernel/libk/string.h
@@ -16,9 +16,15 @@ char* strchr(char* str, int ch);
 void strcpynt(char* dst, char* src, size_t len);
 void strprefcpy(char* dst, char* src, size_t len);
 
+// Copy u8* lower -> u8* lower
+void memcpy_8(u8* dst, u8* src, size_t size);
+// Exact copy of memory (u16, but without skips)
+void memcpy_full(void* dst, void* src, size_t size);
+// Copy like u16* layout (skips every 2nd word)
+void memcpy_16(u16* dst, u16* src, size_t size);
+
 /*
 TODO:
-void memcpy();
 void memmove();
 void memset();
 void memcmp();
