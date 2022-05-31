03788d663d553c9484e4cea9a33559ea820487ad
Piotro <piotro888@wp.pl>
2022-05-31 21:34:22 +0200

    Libk: Add ssize_t and change short->char

--- kernel/libk/types.h
+++ kernel/libk/types.h
@@ -4,20 +4,21 @@
 typedef unsigned int uint16_t;
 typedef int int16_t;
 
-typedef unsigned short uint8_t;
-typedef short int8_t;
+typedef char uint8_t;
+typedef signed char int8_t;
 
 typedef unsigned long uint32_t;
 typedef long int32_t;
 
 typedef unsigned int size_t;
+typedef int ssize_t;
 typedef unsigned int uint_fast_t;
 
 // new types
 typedef unsigned int u16;
 typedef int i16;
-typedef unsigned short u8;
-typedef short i8;
+typedef char u8;
+typedef signed char i8;
 typedef unsigned long u32;
 typedef long i32;
 
