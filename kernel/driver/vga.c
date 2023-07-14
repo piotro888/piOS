becde0d935baaaea40e4bea291d852981b8c9a98
Piotro <piotro888@wp.pl>
2023-07-14 20:58:09 +0200

    driver/VGA: port vga driver

--- kernel/driver/vga.c
+++ kernel/driver/vga.c
@@ -1,9 +1,10 @@
 #include "vga.h"
 #include <libk/types.h>
+#include <proc/virtual.h>
 
-#define VGA_FRAMEBUFF_ADDR 0x0200
-#define VGA_SETTINGS_ADDR 0x4d00
-#define VGA_FAST_SCROLL_ADDR 0x4d02
+#define VGA_FRAMEBUFF_ADDR 0x6000
+#define VGA_SETTINGS_ADDR 0xc000
+#define VGA_FAST_SCROLL_ADDR 0xc002
 
 #define VGA_TEXT_106_48_MODE 1
 
@@ -14,7 +15,9 @@ static int vga_scroll_begin;
 
 void vga_put_char_at(char c, int row, int col) {
     u16 char_value = (u16) c | (vga_text_color<<8);
-    u16* vga_char_addr = (u16*) (VGA_FRAMEBUFF_ADDR + (REAL_ROW(row)*VGA_TEXT_WIDTH*2) + col*2);
+    u16* vga_char_addr = (u16*) (VGA_FRAMEBUFF_ADDR + ((REAL_ROW(row)*VGA_TEXT_WIDTH*2) + col*2));
+    map_page_zero((u16)vga_char_addr>>12);
+    vga_char_addr = (u16*)((u16)vga_char_addr&0x0fff);
     *vga_char_addr = char_value;
 }
 
@@ -26,30 +29,39 @@ void vga_set_color(u8 color) {
 void vga_clear_line(int row) {
     u16* vga_ptr = (u16*) (VGA_FRAMEBUFF_ADDR + (REAL_ROW(row)*VGA_TEXT_WIDTH*2));
     for(int i=0; i<VGA_TEXT_WIDTH; i++) {
-        *vga_ptr++ = 0;
+        map_page_zero((u16)vga_ptr>>12);
+        u16* vga_ptr_p = (u16*)((u16)vga_ptr & 0x0fff);
+        *vga_ptr_p = 0;
+        vga_ptr++;
     }
 }
 
 void vga_clear_screen() {
     u16* vga_ptr = (u16*) VGA_FRAMEBUFF_ADDR;
     for(int i=0; i<VGA_TEXT_WIDTH*VGA_TEXT_HEIGHT; i++) {
-        *vga_ptr++ = 0;
+        map_page_zero((u16)vga_ptr>>12);
+        u16* vga_ptr_p = (u16*)((u16)vga_ptr & 0x0fff);
+        *vga_ptr_p = 0;
+        vga_ptr++;
     }
 }
 
 void vga_fast_scroll() {
     vga_scroll_begin = (vga_scroll_begin+1 >= VGA_TEXT_HEIGHT ? 0 : vga_scroll_begin+1);
-    *(u16*) VGA_FAST_SCROLL_ADDR = vga_scroll_begin;
+    map_page_zero(0xc);
+    *(u16*) 0x2 = vga_scroll_begin;
     vga_clear_line(VGA_TEXT_HEIGHT-1);
 }
 
 void vga_init() {
-    u16* vga_settings = (u16*) VGA_SETTINGS_ADDR;
+    map_page_zero(0xc);
+    u16* vga_settings = (u16*)  0;
     *vga_settings = VGA_TEXT_106_48_MODE;
 
     vga_text_color = 0x0F;
 
     vga_scroll_begin = 0;
-    u16* vga_fast_scroll = (u16*) VGA_FAST_SCROLL_ADDR;
+    u16* vga_fast_scroll = (u16*) 0x2;
     *vga_fast_scroll = vga_scroll_begin;
+    vga_clear_screen();
 }
