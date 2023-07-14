954ca33d552de6dd8a59b34592c33964bbf0b020
Piotro <piotro888@wp.pl>
2023-07-14 20:59:44 +0200

    libk/kprinf: support %lx

--- kernel/libk/kprintf.c
+++ kernel/libk/kprintf.c
@@ -1,4 +1,5 @@
 #include "kprintf.h"
+#include "log.h"
 
 #include <libk/log.h>
 #include <libk/string.h>
@@ -6,7 +7,7 @@
 #define PRINTF_TTY_BUFFER_SIZE 512
 
 /* print unsigned int to ascii buffer */
-int utoa(char* buff, unsigned int num, unsigned int base) {
+int _utoa(char* buff, unsigned int num, unsigned int base) {
     int pos = 0;
 
     if(!num)
@@ -20,7 +21,7 @@ int utoa(char* buff, unsigned int num, unsigned int base) {
         num /= base;
     }
 
-    for(int i=0; i<pos/2; i++) {
+    for(int i=0; i<(pos>>1); i++) {
         char swp = buff[i];
         buff[i] = buff[pos-i-1];
         buff[pos-i-1] = swp;
@@ -32,7 +33,7 @@ int utoa(char* buff, unsigned int num, unsigned int base) {
 
 int vsprintf(char* buff, const char* str, va_list vlist) {
     char* initial_buff = buff;
-
+    
     while(*str) {
         if(*str == '%') {
             str++;
@@ -47,28 +48,51 @@ int vsprintf(char* buff, const char* str, va_list vlist) {
                         i_param *= -1;
                     }
 
-                    buff += utoa(buff, (unsigned int) i_param, 10);
+                    buff += _utoa(buff, (unsigned int) i_param, 10);
                     break;
                 case 'u':
                     str++;
                     i_param = va_arg(vlist, int);
-                    buff += utoa(buff, (unsigned int) i_param, 10);
+                    buff += _utoa(buff, (unsigned int) i_param, 10);
                     break;
                 case 'x':
                     str++;
                     i_param = va_arg(vlist, int);
-                    buff += utoa(buff, (unsigned int) i_param, 16);
+                    buff += _utoa(buff, (unsigned int) i_param, 16);
                     break;
                 case 's':
                     str++;
                     char* s_param = va_arg(vlist, char*);
-                    strcpy(buff, s_param);
-                    buff += strlen(s_param);
+                    buff = strcpyend(buff, s_param);
                     break;
                 case 'c':
                     str++;
                     char c_param = (char)va_arg(vlist, int);
                     *buff++ = c_param;
+                    break;
+                case 'l':
+                    str++;
+                    switch(*str) {
+                        case 'x':
+                            str++;
+                            long l_param = va_arg(vlist, long);
+                            unsigned int h_part = (l_param>>16), l_part = l_param & 0xffff;
+                            if(h_part) {
+                                buff += _utoa(buff, h_part, 16);
+                                char alp[5];
+                                int l = _utoa(alp, l_part, 16);
+                                for(int i=0; i < 4-l; i++)
+                                    *buff++='0';
+                                buff = strcpyend(buff, alp);
+                            } else {
+                                buff += _utoa(buff, l_part, 16);
+                            }
+                            break;
+                        default:
+                            str++;
+                            *buff++ = 'E';
+                            break;
+                    }
                     break;
                 default:
                     *buff++ = 'E';
@@ -94,7 +118,7 @@ int kprintf(const char* str, ...) {
     va_list list;
     va_start(list, str);
 
-    char buff[PRINTF_TTY_BUFFER_SIZE];
+    char buff[PRINTF_TTY_BUFFER_SIZE]; //FIXME: memleak
     int res = vsprintf(buff, str, list);
 
     va_end(list);
