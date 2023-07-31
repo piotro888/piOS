5c9413ce473116fb58fa7b38a32b690431f32bbc
Piotro <piotro888@wp.pl>
2023-08-01 01:33:22 +0200

    Tools: Update send script and add upload target

--- tools/send.py
+++ tools/send.py
@@ -1,23 +1,39 @@
 import serial
 import sys
-from time import sleep
+
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 print(ser.name) 
 
 
-if(len(sys.argv) > 1):
-    f = open(sys.argv[1])
-    data = f.read()
+if(len(sys.argv) > 2):
+    f = open(sys.argv[1], "rb")
+    f2 = open(sys.argv[2], "rb")
+    data_i = f.read()
+    data_d = f2.read()
 else:
-    data = input("send: ") #ach.. this doesn't work when reading > 4095 chars! (see linux termios icannon)
+    print ("Expcected usage: send.py <text bin file> <data bin file>")
+    exit(1)
 
-it = 0; pit=0
-for c in data:
-    ser.write(bytes([ord(c)]))
-    it = it+1
-    if(int(it/(len(data)/100)) != int(pit/(len(data)/100))) :
-        print(int(it/(len(data)/100)), end = " ", flush = True)
-    pit = it
-print("done")
+def send(data, addr):
+    dbytes = len(data)
+    ser.write(ord('m').to_bytes(1, byteorder='little'))
+    ser.write(dbytes.to_bytes(2, byteorder='little'))
+    print(dbytes.to_bytes(2, byteorder='little'))
+    ser.write(addr.to_bytes(4, byteorder='little'))
+    print(addr.to_bytes(4, byteorder='little'))
+    
+    ser.write(data)
 
-ser.close()
+    checksum = 0
+    for i in range(0, dbytes, 2):
+        checksum += int.from_bytes(data[i:i+2], byteorder='little')
+        checksum %= (1<<16)
+    print("cs", hex(checksum)) 
+
+# send program 
+send(data_i, (0x800000 << 1))
+# send data
+send(data_d, (0x100000 << 1) + (0x1000))
+
+# execute
+ser.write(ord('e').to_bytes(1, byteorder='little'))
